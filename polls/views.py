0c902c04d996aa0c9aa770f51da02ccf127c6e12
Can Zhong <don@don.local>
2018-07-25 16:35:07 -0700

    Implemented HTTP 404 ERROR

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from .models import Question
+# Packges and what they do
+from django.shortcuts import render #Easier code
+from django.http import HttpResponse #Respond to requests
+from django.http import Http404 #Throw exception
+from django.template import loader #Loading a template
+from .models import Question #Model
+
 
 def index(request):
     return HttpResponse("Hello, World. You're at the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.obects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -22,4 +29,3 @@ def index(request):
         'latest_question_list' : latest_question_list
     }
     return render(request, 'polls/index.html', context)
-# Create your views here.
